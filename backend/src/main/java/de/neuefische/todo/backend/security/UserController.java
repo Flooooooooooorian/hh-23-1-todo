29e794b6ee347a457fc15acadd719e2a5375d042
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-04-12 10:29:55 +0200

    Add Frontend LoginPage

--- backend/src/main/java/de/neuefische/todo/backend/security/UserController.java
+++ backend/src/main/java/de/neuefische/todo/backend/security/UserController.java
@@ -2,6 +2,7 @@ package de.neuefische.todo.backend.security;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class UserController {
 
     @GetMapping("/me")
     public String getMe() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @PostMapping("/login")
+    public String login() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
