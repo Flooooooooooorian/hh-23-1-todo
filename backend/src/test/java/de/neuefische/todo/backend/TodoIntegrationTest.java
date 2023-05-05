22c7a847754f8a92090f7be97ac175008f03fae0
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-05-05 12:47:55 +0200

    Add Image-Upload Tests

--- backend/src/test/java/de/neuefische/todo/backend/TodoIntegrationTest.java
+++ backend/src/test/java/de/neuefische/todo/backend/TodoIntegrationTest.java
@@ -1,21 +1,31 @@
 package de.neuefische.todo.backend;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.neuefische.todo.backend.todo.Todo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.File;
+import java.util.Collections;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,6 +36,11 @@ public class TodoIntegrationTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @MockBean
+    Cloudinary cloudinary;
+
+    Uploader uploader = mock(Uploader.class);
 
     @Test
     @WithMockUser
@@ -47,19 +62,42 @@ public class TodoIntegrationTest {
     @Test
     @WithMockUser
     void expectSuccessfulPost() throws Exception {
+        MockMultipartFile data = new MockMultipartFile("data",
+                null,
+                MediaType.APPLICATION_JSON_VALUE,
+                """
+                                {
+                                "description":"Nächsten Endpunkt implementieren",
+                                "status":"OPEN"
+                                }
+                        """.getBytes());
+
+        MockMultipartFile file = new MockMultipartFile("file",
+                "testBild.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "testBild".getBytes()
+        );
+
+        File fileToUpload = File.createTempFile("image", null);
+        file.transferTo(fileToUpload);
+
+
+        when(cloudinary.uploader()).thenReturn(uploader);
+        when(uploader.upload(any(), any())).thenReturn(Map.of("url", "test-url"));
+
+
         String actual = mockMvc.perform(
-                        post("http://localhost:8080/api/todo")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("""
-                                        {"description":"Nächsten Endpunkt implementieren","status":"OPEN"}
-                                        """)
+                        multipart("http://localhost:8080/api/todo")
+                                .file(data)
+                                .file(file)
                                 .with(csrf())
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
                         {
                           "description": "Nächsten Endpunkt implementieren",
-                          "status": "OPEN"
+                          "status": "OPEN",
+                          "url": "test-url"
                         }
                         """))
                 .andReturn()
--- backend/src/test/java/de/neuefische/todo/backend/todo/TodoServiceTest.java
+++ backend/src/test/java/de/neuefische/todo/backend/todo/TodoServiceTest.java
@@ -1,17 +1,25 @@
 package de.neuefische.todo.backend.todo;
 
+import com.cloudinary.Uploader;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class TodoServiceTest {
 
     TodoRepository todoRepository = mock(TodoRepository.class);
+    CloudinaryService cloudinaryService = mock(CloudinaryService.class);
 
     TodoService todoService = new TodoService(todoRepository, cloudinaryService);
 
@@ -30,6 +38,26 @@ class TodoServiceTest {
                 .containsExactly(testItem);
     }
 
+    @Test
+    void postTodo() throws IOException {
+        //GIVEN
+        Todo todoToSave = new Todo(null, "description", TodoStatus.OPEN, null);
+        Todo todoToSaveWithId = new Todo("123", "description", TodoStatus.OPEN, null);
+
+
+        when(cloudinaryService.uploadImage(any())).thenReturn("url");
+        when(todoRepository.save(todoToSaveWithId)).thenReturn(todoToSaveWithId);
+        when(uuidService.randomId()).thenReturn("123");
+
+        MultipartFile file = null;
+
+        //WHEN
+        Todo actual = todoService.save(todoToSave, file);
+
+        //THEN
+        Todo expected = new Todo(null, "description", TodoStatus.OPEN, null);
+        assertEquals(expected, actual);
+    }
 }
 
 
